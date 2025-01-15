dc9e856f3988952a9b56a7740885d8844c5089d7
Lucky Yadav <lucky@Luckys-MacBook-Pro.local>
2025-01-15 15:03:22 +0530

    Display address code

---
+++ Displayaddress.java
@@ -0,0 +1,9 @@
+// display your name and complete address in different lines
+public class Displayaddress {
+    public static void main(String[]args) {
+        //printing name and address using escape sequences for new lines
+        System.out.println("Name: John Doe");
+        System.out.println("Address:");
+        System.out.println("123, Baker Street\nLondon, United Kingdom\nZIP: 56789");
+    }
+}
