66d0f5dae2ddc81f12a821fbbd9e067b4da1d52e
Lucky Yadav <lucky@Luckys-MacBook-Pro.local>
2025-01-29 15:47:49 +0530

    code update

--- Divideby6and9.java
+++ Divideby6and9.java
@@ -1,7 +1,7 @@
 public class Divideby6and9 {
     public static void main(String[] args) {
         int sum=0;
-        for (int number=10; number<=37; number++)
+        for (int number=10; number<=950; number++)
         {
             if ( number%6==0 && number%9==0)
             // if (number%18==0) 18 is lcm of 6 and 9 
