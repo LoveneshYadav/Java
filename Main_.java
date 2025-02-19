ecaf8b1afedb401749d8e2c00664d5e838db2a19
Lucky Yadav <lucky@Luckys-MacBook-Pro.local>
2025-02-19 15:33:22 +0530

    lucky

---
+++ Main_.java
@@ -0,0 +1,7 @@
+import Balance.Account;
+public class Main_ {
+    public static void main(String[] args) {
+        Account myAccount = new Account(1820.75);
+        myAccount.Display_Balance();
+    }
+}
